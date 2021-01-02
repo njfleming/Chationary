82a741e2773556f6b8873b3c75835a3b1821e616
njfleming <nathanjfleming@gmail.com>
2021-01-02 13:33:21 -0500

    trying to link connect call to users list

---
+++ src/components/UsersBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UsersBox = ({ room={room}, name={name}, userList={setActiveUsers} }) => {
+    return(
+        <div className="username">
+
+        </div>
+    )
+}
